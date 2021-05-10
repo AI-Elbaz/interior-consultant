0996b50094a66d81ca2f2bcd1a2de03d2b97bcf9
AboodIbrahim <aboodibrahim50@gmail.com>
2021-05-11 01:26:03 +0200

    custom menuBtn when menu is opened

--- js/script.js
+++ js/script.js
@@ -6,10 +6,11 @@ var menuBtn = document.getElementById('menu-btn'),
 mobileStyleWrapper.classList.toggle('mobile', width <= 1200)
 
 menuBtn.addEventListener('click', () => {
-    menuContainer.classList.toggle('collapsed')
+    menuContainer.classList.toggle('collapsed');
+    menuBtn.classList.toggle('opened');
 });
 
 window.addEventListener('resize', (e) => {
     let width = e['target']['innerWidth'];
-    mobileStyleWrapper.classList.toggle('mobile', width <= 998)
+    mobileStyleWrapper.classList.toggle('mobile', width <= 998);
 })
