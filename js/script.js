dc28306ace3582e746ebe3debb79b6d474afb9bc
AboodIbrahim <aboodibrahim50@gmail.com>
2021-05-11 00:53:48 +0200

    responsive navbar

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
-var menuBtn = document.getElementById('menu-btn')
-var menuContainer = document.querySelector('.mobile .container')
+var menuBtn = document.getElementById('menu-btn'),
+    menuContainer = document.querySelector('nav .container'),
+    mobileStyleWrapper = document.getElementsByTagName('nav')[0],
+    width = window.innerWidth;
+
+mobileStyleWrapper.classList.toggle('mobile', width <= 1200)
 
 menuBtn.addEventListener('click', () => {
     menuContainer.classList.toggle('collapsed')
 });
+
+window.addEventListener('resize', (e) => {
+    let width = e['target']['innerWidth'];
+    mobileStyleWrapper.classList.toggle('mobile', width <= 998)
+})
